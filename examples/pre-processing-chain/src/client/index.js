87136e25c734d43bb0082ba018761fc051989953
Benjamin Matuszewski <benjamin.matuszewski@gmail.com>
2017-08-11 15:59:52 +0200

    updated preprocessing chain example

--- examples/pre-processing-chain/src/client/index.js
+++ examples/pre-processing-chain/src/client/index.js
@@ -1,8 +1,8 @@
 import * as lfo from 'waves-lfo/client';
 import * as controllers from 'basic-controllers';
-import * as THREE from 'three';
-import ComplementaryFilter from './lib/ComplementaryFilter';
-import OrientationFilter from './lib/OrientationFilter';
+import Orientation from '../../../../dist/operator/Orientation';
+import Sampler from '../../../../dist/operator/Sampler';
+// import MotionFeatures from '../../../../dist/operator/MotionFeatures';
 
 /**
  * -----------------------------------------------------
@@ -20,83 +20,35 @@ import OrientationFilter from './lib/OrientationFilter';
  * 4 - pitch
  * 3 - roll
  */
-const phoneReceive = new lfo.source.SocketReceive({ port: 5010 });
-phoneReceive.processStreamParams({
+const socketReceive = new lfo.source.SocketReceive({ port: 5010 });
+socketReceive.processStreamParams({
   frameType: 'vector',
   frameSize: 6,
   frameRate: 0,
 });
 
-/**
- * from R-ioT:
- *
- * 0 - filter x
- * 1 - filter y
- * 2 - filter z
- * 3 - acc x
- * 4 - acc y
- * 5 - acc z
- * 6 - roll
- * 7 - pitch
- * 8 - yaw
- */
-const riotReceive = new lfo.source.SocketReceive({ port: 5011 });
-riotReceive.processStreamParams({
-  frameType: 'vector',
-  frameSize: 9,
-  frameRate: 0,
-});
+const sampler = new Sampler({ frameRate: 50 });
+const logger = new lfo.sink.Logger({ time: false, data: true });
 
-/**
- * mapping from phone to R-ioT:
- *
- * 0 - filter x * 1
- * 1 - filter y * 1
- * 2 - filter z * 1
- * 3 - acc x * - 1 / 9.81
- * 4 - acc y * 1 / 9.81
- * 5 - acc z * - 1 / 9.81
- * 8 - roll * -1
- * 7 - pitch * -1
- * 6 - yaw * -1
- */
+
 const defaultK = 0.84;
-const filter = new OrientationFilter({ k: defaultK });
+const orientation = new Orientation({ k: defaultK });
 
-// process input for coherent rendering with R-ioT data
-// must reflect what is done in the filter
-const reMapper = new lfo.operator.Select({
-  indexes: [
-    0,
-    1,
-    2,
-    5,
-    4, // switch 5 and 3
-    3,
-  ],
-});
+// display
+// ---------------------------------------------
 
-const inputScaler = new lfo.operator.Scaler({
-  factor: [
-    -1 / 9.81,
-     1 / 9.81,
-    -1 / 9.81,
-    -1 / 360,
-    -1 / 360,
-    -1 / 360,
-  ]
-});
-
-// map display
 const muteDisplayFactors = [1, 1, 1]; // [1, 0, 1] would be only filter output and raw gyros
 const bpfColors = ['blue', 'green', 'orange'];
 const canvasIds = [
-  '#x-display',
-  '#y-display',
-  '#z-display',
-  '#riot-x-display',
-  '#riot-y-display',
-  '#riot-z-display',
+  '#raw-x-display',
+  '#raw-y-display',
+  '#raw-z-display',
+  '#resampled-x-display',
+  '#resampled-y-display',
+  '#resampled-z-display',
+  '#filtered-x-display',
+  '#filtered-y-display',
+  '#filtered-z-display',
 ];
 
 const displayDefaults = {
@@ -107,55 +59,84 @@ const displayDefaults = {
   height: 150,
 };
 
-// common part of the graph
-phoneReceive.connect(filter);
-phoneReceive.connect(reMapper);
-reMapper.connect(inputScaler);
+socketReceive.connect(sampler);
+sampler.connect(orientation);
 
-const logger = new lfo.sink.Logger({
-  time: false,
-  data: true,
-});
-
-/** Phone
-  * ----------------------------------------------------- */
+//  Raw values
+// ---------------------------------------------
 
 // loop through x, y, z
 for (let i = 0; i < 3; i++) {
-  const filterSelect = new lfo.operator.Select({ index: i });
-  // pick `acc` and `gyro` (cf. reMapper)
-  const selectRawInput = new lfo.operator.Select({ indexes: [i, i + 3] });
-
-  const merger = new lfo.operator.Merger({ frameSizes: [1, 2] });
+  const remapGyros = new lfo.operator.Select({ indexes: [0, 1, 2, 4, 5, 3] });
+  const displayScaler = new lfo.operator.Scaler({
+    factor: [
+      1 / 9.81,
+      1 / 9.81,
+      1 / 9.81,
+      1 / 360,
+      1 / 360,
+      1 / 360,
+    ]
+  });
+  const select = new lfo.operator.Select({ indexes: [i, i + 3]})
   const muteDisplay = new lfo.operator.Scaler({ factor: muteDisplayFactors });
   const display = new lfo.sink.BpfDisplay(Object.assign(displayDefaults, {
     canvas: canvasIds[i],
     colors: bpfColors,
   }));
 
-  filter.connect(filterSelect);
-  filterSelect.connect(merger);
+  socketReceive.connect(remapGyros);
+  remapGyros.connect(displayScaler);
+  displayScaler.connect(select);
 
-  inputScaler.connect(selectRawInput);
-  selectRawInput.connect(merger);
-
-  merger.connect(muteDisplay);
+  select.connect(muteDisplay);
   muteDisplay.connect(display);
 }
 
-/** R-ioT
-  * ----------------------------------------------------- */
+//  Resampled values
+// ---------------------------------------------
 
 // loop through x, y, z
 for (let i = 0; i < 3; i++) {
-  const select = new lfo.operator.Select({ indexes: [i, i + 3, i + 6] });
+  const remapGyros = new lfo.operator.Select({ indexes: [0, 1, 2, 4, 5, 3] });
+  const displayScaler = new lfo.operator.Scaler({
+    factor: [
+      1 / 9.81,
+      1 / 9.81,
+      1 / 9.81,
+      1 / 360,
+      1 / 360,
+      1 / 360,
+    ]
+  });
+  const select = new lfo.operator.Select({ indexes: [i, i + 3]})
   const muteDisplay = new lfo.operator.Scaler({ factor: muteDisplayFactors });
-    const display = new lfo.sink.BpfDisplay(Object.assign(displayDefaults, {
+  const display = new lfo.sink.BpfDisplay(Object.assign(displayDefaults, {
     canvas: canvasIds[i + 3],
     colors: bpfColors,
   }));
 
-  riotReceive.connect(select);
+  sampler.connect(remapGyros);
+  remapGyros.connect(displayScaler);
+  displayScaler.connect(select);
+
+  select.connect(muteDisplay);
+  muteDisplay.connect(display);
+}
+
+//  Resampled values
+// ---------------------------------------------
+
+// loop through x, y, z
+for (let i = 0; i < 3; i++) {
+  const select = new lfo.operator.Select({ indexes: [i]})
+  const muteDisplay = new lfo.operator.Scaler({ factor: muteDisplayFactors });
+  const display = new lfo.sink.BpfDisplay(Object.assign(displayDefaults, {
+    canvas: canvasIds[i + 6],
+    colors: bpfColors,
+  }));
+
+  orientation.connect(select);
   select.connect(muteDisplay);
   muteDisplay.connect(display);
 }
@@ -166,36 +147,15 @@ for (let i = 0; i < 3; i++) {
  * -----------------------------------------------------
  */
 
-const selectFilter = new controllers.Toggle({
-  label: 'display filter',
-  active: true,
-  container: '#controls',
-  callback: value => muteDisplayFactors[0] = value + 0,
-});
-
-const selectAcc = new controllers.Toggle({
-  label: 'display acc',
-  active: true,
-  container: '#controls',
-  callback: value => muteDisplayFactors[1] = value + 0,
-});
-
-const selectGyro = new controllers.Toggle({
-  label: 'display gyro',
-  active: true,
-  container: '#controls',
-  callback: value => muteDisplayFactors[2] = value + 0,
-});
-
 const kslider = new controllers.Slider({
-  label: 'filter k factor',
+  label: 'orientation k factor',
   min: 0,
   max: 1,
   step: 0.01,
   default: defaultK,
   size: 'large',
   container: '#controls',
-  callback: value => filter.params.set('k', value),
+  callback: value => orientation.params.set('k', value),
 });
 
 
--- examples/pre-processing-chain/src/client/mobile.js
+++ examples/pre-processing-chain/src/client/mobile.js
@@ -1,60 +1,13 @@
 import * as lfo from 'waves-lfo/client';
-import motionInput from 'motion-input';
+import MotionInput from '../../../../dist/source/MotionInput';
+
+const motionInput = new MotionInput();
+const socketSend = new lfo.sink.SocketSend({ port: 5000 });
+const logger = new lfo.sink.Logger({ time: false, data: true });
+
+motionInput.connect(socketSend);
 
 motionInput
-  .init(['accelerationIncludingGravity', 'rotationRate'])
-  .then(([accelerationIncludingGravity, rotationRate]) => {
-
-    // @todo - replace with lfo.source.MotionInput
-
-    const eventIn = new lfo.source.EventIn({
-      frameType: 'vector',
-      frameSize: 6,
-      frameRate: 1 / accelerationIncludingGravity.period,
-    });
-
-    const socketSend = new lfo.sink.SocketSend({ port: 5000 });
-
-    eventIn.connect(socketSend);
-    eventIn.init().then(() => {
-      eventIn.start();
-      let data = [];
-
-      /**
-       * send:
-       *
-       * 0 - acc x
-       * 1 - acc y
-       * 2 - acc z
-       * 5 - yaw
-       * 4 - pitch
-       * 3 - roll
-       */
-      if (accelerationIncludingGravity.isValid && rotationRate.isValid) {
-        accelerationIncludingGravity.addListener(([x, y, z]) => {
-          data[0] = x;
-          data[1] = y;
-          data[2] = z;
-        });
-
-        rotationRate.addListener(([alpha, beta, gamma]) => {
-          data[3] = alpha;
-          data[4] = beta;
-          data[5] = gamma;
-
-          eventIn.process(null, data);
-        });
-      } else if (accelerationIncludingGravity.isValid) {
-        accelerationIncludingGravity.addListener(([x, y, z]) => {
-          data[0] = x;
-          data[1] = y;
-          data[2] = z;
-          data[3] = 0;
-          data[4] = 0;
-          data[5] = 0;
-
-          eventIn.process(null, data);
-        });
-      }
-    });
-  });
+  .init()
+  .then(() => motionInput.start())
+  .catch(err => console.log(err.stack));
